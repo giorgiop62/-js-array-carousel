501d28493523005c0120891ff620e70cf4662269
giorgip62 <giorgiop62@gmail.com>
2022-10-17 18:16:31 +0200

    arrayImg

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const imgArray= ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+
+//stringa vuota che conterrà tutto l'elenco dei tag
+let images ='';
+
+const box = document.querySelector('.box');
+
+//ciclo:
+
+for(i = 0; i < imgArray.length; i++){
+  
+}
+
+
